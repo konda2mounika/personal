1d3299b1a26a632b019203e5267c1f981d52c105
mounika konda <konda2mounika@gmail.com>
2023-10-10 19:27:04 +0530

    0001

--- Python/iam_list_users.py
+++ Python/iam_list_users.py
@@ -3,7 +3,7 @@ import boto3
 # Open Management Console
 aws_management_console = boto3.session.Session(profile_name="default")
 # Open IAM Console
-iam_console = aws_managament_console.client(service_name="iam")
+iam_console = aws_management_console.client(service_name="iam")
 # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
 result =iam_console.list_users()
 print(result)
