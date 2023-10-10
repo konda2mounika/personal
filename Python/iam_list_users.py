62aa772d02a556ff0084cd1fa52793c263d09b1f
mounika konda <konda2mounika@gmail.com>
2023-10-10 19:19:18 +0530

    0001

--- Python/iam_list_users.py
+++ Python/iam_list_users.py
@@ -1,7 +1,7 @@
 # Import all the modules and Libraries
 import boto3
 # Open Management Console
-aws_management_console = boto3.session.session(profile_name = "default")
+aws_management_console = boto3.session.Session(profile_name = "default")
 # Open IAM Console
 iam_console = aws_managament_console.client(service_name="iam")
 # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
