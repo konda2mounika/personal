d9a54b40a395b7fd6b97ddc1f3905ace7c4e47cd
mounika konda <konda2mounika@gmail.com>
2023-10-10 20:59:25 +0530

    0001

--- Python/launch_instance.py
+++ Python/launch_instance.py
@@ -3,7 +3,7 @@ import boto3
 
 aws_management_console = boto3.session.Session(profile_name="default")
 
-ec2_console = aws_management_console.client("ec2")
+ec2_console = aws_management_console.client(service_name="ec2")
 
 response = ec2_console.run_instances(
     ImageId = 'ami-065680f8cbec51c69',
