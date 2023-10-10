a64987a8a62cc75843957fc4e2725d41124651d5
mounika konda <konda2mounika@gmail.com>
2023-10-10 18:56:00 +0530

    0001

--- Python/my-first-boto3-script.py
+++ Python/my-first-boto3-script.py
@@ -1,6 +1,6 @@
 import boto3
 
-aws_management_console = boto3.session.Session(profile_name="Konda Srikanth")
+aws_management_console = boto3.session.Session
 iam_console_resource = aws_management_console.resource('iam') 
 
 
