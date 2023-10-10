f4cc16c84174c40d6d6cc76533e86c4042bbe00f
mounika konda <konda2mounika@gmail.com>
2023-10-10 20:02:33 +0530

    0001

--- Python/list_ec2_instances.py
+++ Python/list_ec2_instances.py
@@ -1,9 +1,10 @@
 # Import all the modules and Libraries
 import boto3
+from pprint import pprint
 # Open Management Console
 aws_management_console = boto3.session.Session(profile_name="default")
 # Open EC2 Console
 ec2_console = aws_management_console.client(service_name="ec2")
 # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
 result = ec2_console.describe_instances()['Reservations']
-print(result)
+pprint(result)
