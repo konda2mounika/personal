23cd007ef8c06c7d960d0e207276677890e07af6
mounika konda <konda2mounika@gmail.com>
2023-10-10 20:01:05 +0530

    0001

--- Python/list_ec2_instances.py
+++ Python/list_ec2_instances.py
@@ -6,6 +6,4 @@ aws_management_console = boto3.session.Session(profile_name="default")
 ec2_console = aws_management_console.client(service_name="ec2")
 # Use Boto3 Documentation to get more information (https://boto3.amazonaws.com/v1/documentation/api/latest/index.html)
 result = ec2_console.describe_instances()['Reservations']
-for each_instance in result:
-    for value in each_instance['Instances']:
-        print(value['InstanceId'])
+print(result)
