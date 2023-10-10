e893b2eb09d592f913e7892180d1aec5c23b317d
mounika konda <konda2mounika@gmail.com>
2023-10-10 20:47:19 +0530

    0001

--- Python/launch_instance.py
+++ Python/launch_instance.py
@@ -6,8 +6,8 @@ aws_management_console = boto3.session.Session(profile_name="default")
 iam_console = aws_management_console.client("ec2")
 
 response = ec2_console.run_instances(
-    ImageId = "ami-065680f8cbec51c69",
-    InstanceType= "t2.micro",
+    ImageId = 'ami-065680f8cbec51c69',
+    InstanceType= 't2.micro',
     maxCount=1,
     minCount=1
 )
