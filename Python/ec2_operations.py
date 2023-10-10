7446cf90b57980ba752f2fb9cdcf0fc097dd159e
mounika konda <konda2mounika@gmail.com>
2023-10-10 21:04:21 +0530

    0001

--- Python/ec2_operations.py
+++ Python/ec2_operations.py
@@ -4,7 +4,7 @@ aws_management_console = boto3.session.Session(profile_name="default")
 
 ec2_console = aws_management_console.client(service_name ="ec2")
 
-response = ec2_console.start_instances(
+response = ec2_console.terminate_instances(
     InstanceIds= ['i-07c2c18b9d7f5ec74']
 )
    
